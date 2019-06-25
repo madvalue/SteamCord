b643e31e3ac6c4ce12184da4564e67667f4afce7
Maciej Uliszewski <value2k@gmail.com>
2019-06-25 14:27:00 +0200

    Loading first module

--- main.js
+++ main.js
@@ -1,9 +1,4 @@
 require("dotenv").config();
 
 require("./src/Discord.js");
-
-(async () => {
-    const Profiles = require("./src/Steam/Profiles.js");
-    console.log(await Profiles.usernameToID("maciegtoja"));
-    console.log(await Profiles.usernameToID("maciegtoja"));
-})();
+require("./src/Modules/Profiles.js");
